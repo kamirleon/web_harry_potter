3bb8dab98e8dc6491c3b43cfa18dbb1aa0f732b7
LuisSanMartinFoster <lsanmartinfoster@gmail.com>
2023-04-26 23:57:30 -0400

    Login arreglado
    Se ha arreglado el login

---
+++ Login/Login.js
@@ -0,0 +1,30 @@
+var correo = document.getElementById('idCorreo');
+var contrasena = document.getElementById('idContrasena');
+var error = document.getElementById('error');
+error.style.color = 'red';
+
+function enviarFormulario(){
+    console.log('Enviando Formulario');
+    var mensajesError = [];
+    if (correo.value === null || correo.value === ''){
+        mensajesError.push('Ingresa tu Correo');
+    }
+
+    
+    if (contrasena.value === null || contrasena.value === ''){
+       mensajesError.push('Ingresa tu contraseña');
+    }
+
+    console.log(mensajesError);
+    error.innerHTML = mensajesError.join(', ');
+
+    // error.innerHTML = mensajesError.join(', ');
+    correo.innerHTML = mensajesError.join(', ');
+    return false;
+
+}
+
+// var formulario = document.getElementById('formulario');
+//     formulario.addEventListener('Submit ',function(event){
+//         console.log('Enviando Formulario');
+//     })
